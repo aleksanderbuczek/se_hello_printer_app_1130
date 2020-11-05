cef52fd2a7009d31ddbac09aeea14172b7279ead
vdi-student <vdi-student@EDU-WSB.local>
2020-11-05 12:31:11 +0100

    Bugfix

--- test/test_views.py
+++ test/test_views.py
@@ -15,5 +15,5 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Alek", "nazwisko":"Buczek", "mgs":"Hello World!"}',
+        self.assertEqual(b'{ "imie":"Aleksander", "nazwisko":"Buczek", "mgs":"Hello World!"}',
                          rv.data)
