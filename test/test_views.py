0e05da8f4ed94aa7febc04a8375030cb8a28fbc0
vdi-student <vdi-student@EDU-WSB.local>
2020-11-05 14:16:04 +0100

    LINT FIX :)

--- test/test_views.py
+++ test/test_views.py
@@ -15,7 +15,6 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(
-                         b'{"imie": "Aleksander", "nazwisko":' +
-                         '"Buczek", "mgs": "Hello World!"}',
+        self.assertEqual(b"""{"imie":"Aleksander",
+                         "nazwisko":"Buczek", "mgs":"Hello World!"}""",
                          rv.data)
